e5096375acc978a1fdf093ee986d29d67ae19cc2
Lucas Cruz <lucas_ftc@hotmail.com>
2019-02-22 00:58:53 +0000

    add sign up logic and page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  
+  def new
+  end
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/', flash: { success: 'User created successfully!' }
+    else
+      redirect_to '/signup', flash: { warning: 'Invalid Email and Password'}
+    end
+  end
+
+  private
+
+  def user_params
+    # add first and last name
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
+end
