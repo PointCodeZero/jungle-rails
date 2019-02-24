249c2cba16440c06ac1ad08cd5f7cad0823b9871
Lucas Cruz <lucas_ftc@hotmail.com>
2019-02-24 18:25:30 +0000

    DRY up code

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,26 @@
 class ReviewsController < ApplicationController
   before_action :find_product
 
+  def create
+    review = Review.new(review_params)
+    review.product_id = params[:product_id]
+    review.user_id = session[:user_id]
+    
+    if review.save
+      review.save
+      redirect_to "/products/#{@product.id}"
+    else
+      redirect_to "/products/#{@product.id}"
+    end
+  end
+
   private
 
   def find_product
     @product = Product.find params[:product_id]
+  end
+
+  def review_params
+    params.require(:review).permit(:rating, :description)
   end
 end
