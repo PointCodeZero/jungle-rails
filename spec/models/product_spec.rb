a7e86dc438b7f6a4c2b42dc602134bb59f09131b
Lucas Cruz <lucas_ftc@hotmail.com>
2019-02-25 19:36:11 +0000

    DRY validation tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,29 +2,61 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
+    it 'is valid' do
+      cat = Category.new(:name => true)
+      prod = Product.new(
+        :name => 'Pencil', 
+        :price => 1, 
+        :quantity => 500, 
+        :category => cat
+      )
+      expect(prod).to be_valid
+      expect(prod.errors.full_messages).to include()
+    end
+
     it 'is not valid without a name' do
       cat1 = Category.new(:name => true)
-      prod1 = Product.new(:name => nil, :price => 40, :quantity => 34, :category => cat1)
+      prod1 = Product.new(
+        :name => nil, 
+        :price => 40, 
+        :quantity => 34, 
+        :category => cat1
+      )
       expect(prod1).to_not be_valid
       expect(prod1.errors.full_messages).to include("Name can't be blank")
     end
 
     it 'is not valid without a price' do
       cat2 = Category.new(:name => true)
-      prod2 = Product.new(:name => 'Hat', :price => nil, :quantity => 34, :category => cat2)
+      prod2 = Product.new(
+        :name => 'Hat', 
+        :price => nil, 
+        :quantity => 34, 
+        :category => cat2
+      )
       expect(prod2).to_not be_valid
       expect(prod2.errors.full_messages).to include("Price can't be blank")
     end
 
     it 'is not valid without a quantity' do
       cat3 = Category.new(:name => true)
-      prod3 = Product.new(:name => 'Book', :price => 25, :quantity => nil, :category => cat3)
+      prod3 = Product.new(
+        :name => 'Book', 
+        :price => 25, 
+        :quantity => nil, 
+        :category => cat3
+      )
       expect(prod3).to_not be_valid
       expect(prod3.errors.full_messages).to include("Quantity can't be blank")
     end
 
     it 'is not valid without a category' do
-      prod4 = Product.new(:name => 'Umbrela', :price => 13, :quantity => 135, :category => nil)
+      prod4 = Product.new(
+        :name => 'Umbrela', 
+        :price => 13, 
+        :quantity => 135, 
+        :category => nil
+      )
       expect(prod4).to_not be_valid
       expect(prod4.errors.full_messages).to include("Category can't be blank")
     end
